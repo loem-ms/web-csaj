2078cbe0c347e32d0becf8dc6e45a68a0db842ef
Mengsay Loem <loem.m.aa@m.titech.ac.jp>
2023-01-24 23:36:38 +0900

    init nextjs app

---
+++ csaj-page/pages/news.js
@@ -0,0 +1,5 @@
+export default function News() {
+    return <div>
+        <h1>News from Us</h1>
+    </div>
+  }
---
+++ csaj-page/pages/tips.js
@@ -0,0 +1,7 @@
+export default function Tips() {
+    return <div>
+        <h1>
+            Resources
+        </h1>
+    </div>
+  }
