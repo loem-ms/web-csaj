a21d183fc924df7965c9b0dc8d919a25deaebd53
Mengsay Loem <loem.m.aa@m.titech.ac.jp>
2023-01-27 00:15:41 +0900

    render markdown

--- csaj-page/pages/news.js
+++ csaj-page/pages/news.js
@@ -1,7 +1,40 @@
-export default function News() {
-    return <div>
-        <h1>News from Us</h1>
-        <h2>Upcoming Events</h2>
-        <h2>Past Events</h2>
-    </div>
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Date from '../lib/date';
+
+export default function News({ allPostsData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>CSAJ-News</title>
+      </Head>
+      
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Recent Activities</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
   }
+}
